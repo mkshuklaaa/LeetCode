48dab3846668a084a138269db9ccff1ce2ffd97b
Manish Kumar Shukla <148890843+mkshuklaaa@users.noreply.github.com>
2025-08-26 04:25:12 +0530

    Time: 5 ms (27.05%), Space: 47.9 MB (31.72%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -1,21 +1,15 @@
 class Solution {
-    public int max(int[][] mat,int r,int c,int[][] dp){
-        if(r==mat.length-1 && c==mat[0].length-1) return mat[r][c];
-        if(r>=mat.length || c>=mat[0].length) return Integer.MAX_VALUE;
-        if(r<0 || c<0) return 0;
-        if(dp[r][c]!=-1) return dp[r][c];
-        int d = max(mat,r+1,c,dp);
-        int dr = max(mat,r,c+1,dp);
-        return dp[r][c] = mat[r][c] + Math.min(d, dr);
-    }
     public int minPathSum(int[][] mat) {
         int r = mat.length;
         int c = mat[0].length;
-        int[][] dp = new int[r+1][c+1];
-        for(int i=0;i<=r;i++){
-            for(int j=0;j<=c;j++) dp[i][j]=-1;
-        }
-        return max(mat,0,0,dp);
+        int[][] dp = new int[r][c];
         
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                if(i==0 && j==0) dp[i][j] = mat[i][j];
+                else dp[i][j] = mat[i][j] + Math.min((i-1>=0)?dp[i-1][j]:Integer.MAX_VALUE,(j-1>=0)?dp[i][j-1]:Integer.MAX_VALUE);
+            }
+        }
+        return dp[r-1][c-1];
     }
 }
