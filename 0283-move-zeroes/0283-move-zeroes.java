7a265b15ab6b6baa97bde4e5e1e2c1b3a6849642
Manish Kumar Shukla <148890843+mkshuklaaa@users.noreply.github.com>
2025-09-19 15:40:49 +0530

    Time: 2 ms (78.9%), Space: 46 MB (79.5%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,19 +1,16 @@
 class Solution {
     public void moveZeroes(int[] nums) {
+        int l = 0;
         int n = nums.length;
-        int count = 0;
-        for(int i=0;i<n;i++) if(nums[i]==0) count++;
 
-        for(int j=0;j<count;j++){
-            for(int i=1;i<n;i++){
-                if(nums[i-1]==0){
-                    int temp = nums[i];
-                    nums[i] = nums[i-1];
-                    nums[i-1] = temp;
-                }
+        for(int r=0;r<n;r++){
+            if(nums[r]!=0){
+                int temp = nums[l];
+                nums[l] = nums[r];
+                nums[r] = temp;
+                l++;
             }
         }
-        
         
     }
 }
